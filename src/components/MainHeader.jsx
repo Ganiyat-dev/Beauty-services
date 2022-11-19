36da60720f6f25bdd6e90291295f709469b38b93
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2022-11-19 08:33:34 +0100

    refactor: navbar section

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const MainHeader = () => {
+  return (
+    <header className='main__header'>
+        <div className='container main__header-container'>
+            <div className='main__header-left'>
+                <h4>Welcome to GlamUp</h4>
+                <h1> Made for professionals by professionals </h1>
+                <Link to="/plans" className='btn lg'>Reserve your Spot</Link>
+            </div>
+
+
+        </div>
+
+    </header>
+  )
+}
+
+export default MainHeader
